260b22212c8f17a291d52a8d3d2e6b84dfb2bf3e
tahee143 <devtaehee@naver.com>
2024-07-04 05:33:32 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력받기
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a - b);
+    }
+}
