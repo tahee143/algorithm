81334f5ca90ed92da4839afbb89656c7f5afe61b
tahee143 <devtaehee@naver.com>
2024-07-10 06:11:01 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 100 ms, Memory: 14048 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * MenOfPassion(A[], n) {
+        *    i = ⌊n / 2⌋;
+        *    return A[i]; # 코드1
+        * }
+        * MenOfPassion 알고리즘은 어떤 수가 들어와도 한번만 실행
+        * 수행시간은 O(1)
+        * */
+
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
