f19d7e01d113c5276f4dfbc5038a124e5b7fd753
tahee143 <devtaehee@naver.com>
2024-07-10 05:40:52 +0900

    [Bronze V] Title: 합, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ Java/백준/Bronze/8393. 합/합.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+
+        bw.append(String.valueOf(sum));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
