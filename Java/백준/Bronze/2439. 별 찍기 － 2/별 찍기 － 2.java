0c0461402a6c75b7a464186b37c31f5cfeb7476c
tahee143 <devtaehee@naver.com>
2024-07-10 01:28:48 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n-i; j++) {
+                bw.append(" ");
+            }
+            for(int k = 1; k <= i; k++) {
+                bw.append("*");
+            }
+            bw.append("\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
