9c765e28095fc4b41ee401d3af2c8d13a174ecb0
tahee143 <devtaehee@naver.com>
2024-07-13 06:47:06 +0900

    [Bronze I] Title: 단어 공부, Time: 132 ms, Memory: 14128 KB -BaekjoonHub

--- Java/백준/Bronze/1157. 단어 공부/단어 공부.java
+++ Java/백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,34 +1,28 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        br.close();
-
-        s = s.toUpperCase(); // 대문자로 변환
-        int[] c = new int[26];
-
-        for(int i = 0; i < s.length(); i++){ // 아스키코드 이용 A - A = 0 0번 배열 증가
-            int n = s.charAt(i) - 'A';
-            c[n]++;
+        int[] arr = new int[26];
+        int c = System.in.read(); // 버퍼(inputStream) 사용해 효율적, 문자를 하나씩 가져와 아스키 코드로 변환
+        while (c > 64) {
+            if (c < 91) {
+                arr[c - 65]++;
+            } else {
+                arr[c - 97]++;
+            }
+            c = System.in.read();
         }
 
         int max = -1;
-        int result = -2; // ? 아스키코드 -2
-        for(int i = 0; i < 26; i++){
-            if(c[i] > max) {
-                max = c[i];
-                result = i;
-            } else if (c[i] == max) {
-                result = -2;
+        int ch = -2; // ? 아스키 코드 값 63
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = i;
+            } else if (arr[i] == max) {
+                ch = -2;
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append((char)(result + 65));
-        System.out.println(sb);
+        System.out.println((char) (ch + 65));
     }
 }
