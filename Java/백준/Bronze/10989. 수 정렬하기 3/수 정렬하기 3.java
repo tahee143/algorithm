1455ef7e2c303a6b5e2ba77025af06bbff7f2ff6
tahee143 <devtaehee@naver.com>
2024-07-24 10:45:57 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1548 ms, Memory: 336128 KB -BaekjoonHub

--- Java/백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ Java/백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
@@ -11,16 +10,21 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
+        // 최대 숫자 10000
+        int[] arr = new int[10001];
 
+        // 입력받은 숫자 체크하기
         for(int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(br.readLine());
+            arr[num]++;
         }
 
-        Arrays.sort(arr);
-
-        for(int num : arr){
-            sb.append(num).append("\n");
+        // 입력받은 숫자 출력
+        for(int i = 1; i <= 10000; i++){
+            while (arr[i] > 0){ // 0보다 크면 입력 받았음 의미, 1보다 큰경우 중복된 경우
+                sb.append(i).append("\n");
+                arr[i]--;
+            }
         }
 
         System.out.println(sb);
